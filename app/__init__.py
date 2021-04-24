39f87c113ea8ff86fae2d2280231c9161a2c421b
Vinay Pandey <vpandey@rms.com>
2021-04-24 11:36:37 -0700

    Add new code + IDE settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,93 @@
-import flask,requests,json 
-import jinja2
-from authlib.flask.client import OAuth
+import flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = flask.Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 'False'
+db = SQLAlchemy(app)
+
+
+class Info(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    temp = db.Column(db.String(5), unique=False, nullable=False)
+    status = db.Column(db.String(3), unique=False, nullable=False)
+
+
 @app.route("/")
 def home_view():
-    return "<p>hello</p>"
+    to_return = ""
+    allInfo = Info.query.all()
+    for infoBit in allInfo:
+        to_return += "<p> Status: %s, Temperature: %s</p><br/>" % (infoBit.status, infoBit.temp)
+    return to_return
 
 
-@app.route("/abc")
-def abc():
-    return jsonify(
-                message="good test",
-                category="success",
-                data=data,
-                status=200
-            )
+@app.route("/latest.html", methods=["GET"])
+def latest():
+    latest_info = Info.query.order_by(Info.id.desc()).first()
+    if latest_info is None:
+        return flask.jsonify(info="No Data", status=200)
+    else:
+        return flask.jsonify(info=latest_info.status + ";" + latest_info.temp, status=200)
+
+
+@app.route("/toggle")
+def toggle_manual():
+    old_info = Info.query.order_by(Info.id.desc()).first()
+    if old_info is None:
+        temperature = "0"
+        stat = "0"
+    else:
+        temperature = old_info.temp
+        stat = old_info.status
+
+    if stat == "0":
+        stat = "1"
+    else:
+        stat = "0"
+
+    new_info = Info(temp=temperature, status=stat)
+    db.session.add(new_info)
+    db.session.commit()
+
+    return flask.jsonify(status=200)
+
+
+@app.route("/toggle.html", methods=["POST"])
+def toggle():
+    old_info = Info.query.order_by(Info.id.desc()).first()
+    if old_info is None:
+        temperature = "0"
+        stat = "0"
+    else:
+        temperature = old_info.temp
+        stat = old_info.status
+
+    if stat == "0":
+        stat = "1"
+    else:
+        stat = "0"
+
+    new_info = Info(temp=temperature, status=stat)
+    db.session.add(new_info)
+    db.session.commit()
+
+    return flask.jsonify(status=200)
+
+
+@app.route("/temp.html", methods=["POST"])
+def temp():
+    new_temp = list(flask.request.form.keys())[0]
+    old_info = Info.query.order_by(Info.id.desc()).first()
+
+    if old_info is None:
+        stat = "0"
+    else:
+        stat = old_info.status
+
+    new_info = Info(temp=new_temp, status=stat)
+    db.session.add(new_info)
+    db.session.commit()
+
+    return flask.jsonify(status=200)
